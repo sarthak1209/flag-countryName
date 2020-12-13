9dc09d9a787742b043064d441387a44fde50c94c
Sarthak Lav <sarthakchaudhary52@gmail.com>
2020-12-13 22:01:37 +0530

    Added : working with emojis

--- flag-name/src/App.js
+++ flag-name/src/App.js
@@ -10,19 +10,38 @@ var flagsDictionary = {
   "❤️": "love",
   "👜": "purse"
 };
+
+var flags = Object.keys(flagsDictionary);
+
 export default function App() {
-  var [country, setCountry] = useState("");
+  var [country, setCountry] = useState("Country name will be shown here");
   function onEmojiEnter(event){
     var country = flagsDictionary[event.target.value];
     setCountry(country);
+  }
+
+  function onClickEmoji(flag){
+     var countryName = flagsDictionary[flag];
+    console.log(flag); 
+    console.log(countryName);
+    setCountry(countryName);
 
   }
   return (
     <div className="App">
       <h1>Not sure which flag belong to which country?</h1>
-      <h2>Enter or click on the flag to know about this</h2>
-        <input onChange={onEmojiEnter}  />
+      <h2>Enter or click on the flag to know</h2>
+        <input style={{margin:"2rem"}} key={country} onChange={onEmojiEnter}  />
         <div>{country}</div>
+
+     {
+       flags.map(flag=>{
+
+          return <span onClick={()=>onClickEmoji(flag)}  key={flag}
+          style={{fontSize:"2rem", padding:"1rem", cursor:"pointer"}}>{flag }</span>
+       })
+     }
+      
 
     </div>
   );
