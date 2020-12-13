6fa9060922ad8bcbbe4f8e3cc544e551a4162d66
Sarthak Lav <sarthakchaudhary52@gmail.com>
2020-12-13 20:34:35 +0530

    added: working with input (emoji dict)

--- flag-name/src/App.js
+++ flag-name/src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./index.css";
 
-
+var flagsDictionary = {
+  "😊": "smiling",
+  "😒": "disbelief",
+  "😂": "Tears of joy",
+  "✌🏻": "victory or friendship",
+  "👌🏻": "Looks good",
+  "❤️": "love",
+  "👜": "purse"
+};
 export default function App() {
-  
+  var [country, setCountry] = useState("");
+  function onEmojiEnter(event){
+    var country = flagsDictionary[event.target.value];
+    setCountry(country);
+
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Not sure which flag belong to which country?</h1>
+      <h2>Enter or click on the flag to know about this</h2>
+        <input onChange={onEmojiEnter}  />
+        <div>{country}</div>
 
     </div>
   );
